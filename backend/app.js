9a6c06a7dff7898605f3505c7e66e86aef95f4a2
Samiullah <samiullahk997@gmail.com>
2023-10-29 05:15:15 +0500

    update app.js file

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 // app.use(
 //   cors({
@@ -14,12 +15,13 @@ const cors = require("cors");
 // );
 
 app.use(cors({
-  origin: ['https://haramsales.vercel.app',],
+  origin: ['http://localhost:3000',],
   credentials: true
 }));
 
 app.use(express.json());
 app.use(cookieParser());
+app.use("/", express.static(path.join(__dirname,"./uploads")));
 app.use("/test", (req, res) => {
   res.send("Hello world!");
 });
