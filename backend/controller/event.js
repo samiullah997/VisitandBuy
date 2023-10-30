45796482579bd54d0b74ee87964bb4466573f5db
Samiullah <samiullahk997@gmail.com>
2023-10-30 05:16:14 +0500

    Update event.js file

--- backend/controller/event.js
+++ backend/controller/event.js
@@ -6,10 +6,14 @@ const ErrorHandler = require("../utils/ErrorHandler");
 const { isSeller, isAdmin, isAuthenticated } = require("../middleware/auth");
 const router = express.Router();
 const cloudinary = require("cloudinary");
+const { upload } = require("../multer");
+const fs = require("fs");
+
 
 // create event
 router.post(
   "/create-event",
+  upload.array("images"),
   catchAsyncErrors(async (req, res, next) => {
     try {
       const shopId = req.body.shopId;
@@ -17,36 +21,18 @@ router.post(
       if (!shop) {
         return next(new ErrorHandler("Shop Id is invalid!", 400));
       } else {
-        let images = [];
+        const files = req.files;
+        const imageUrls = files.map((file) => `${file.filename}`);
 
-        if (typeof req.body.images === "string") {
-          images.push(req.body.images);
-        } else {
-          images = req.body.images;
-        }
+        const eventData = req.body;
+        eventData.images = imageUrls;
+        eventData.shop = shop;
 
-        const imagesLinks = [];
-
-        for (let i = 0; i < images.length; i++) {
-          const result = await cloudinary.v2.uploader.upload(images[i], {
-            folder: "products",
-          });
-
-          imagesLinks.push({
-            public_id: result.public_id,
-            url: result.secure_url,
-          });
-        }
-
-        const productData = req.body;
-        productData.images = imagesLinks;
-        productData.shop = shop;
-
-        const event = await Event.create(productData);
+        const product = await Event.create(eventData);
 
         res.status(201).json({
           success: true,
-          event,
+          product,
         });
       }
     } catch (error) {
@@ -90,19 +76,26 @@ router.delete(
   "/delete-shop-event/:id",
   catchAsyncErrors(async (req, res, next) => {
     try {
-      const event = await Event.findById(req.params.id);
+      const productId = req.params.id;
 
-      if (!product) {
-        return next(new ErrorHandler("Product is not found with this id", 404));
-      }    
+      const eventData = await Event.findById(productId);
 
-      for (let i = 0; 1 < product.images.length; i++) {
-        const result = await cloudinary.v2.uploader.destroy(
-          event.images[i].public_id
-        );
+      eventData.images.forEach((imageUrl) => {
+        const filename = imageUrl;
+        const filePath = `uploads/${filename}`;
+
+        fs.unlink(filePath, (err) => {
+          if (err) {
+            console.log(err);
+          }
+        });
+      });
+
+      const event = await Event.findByIdAndDelete(productId);
+
+      if (!event) {
+        return next(new ErrorHandler("Event not found with this id!", 500));
       }
-    
-      await event.remove();
 
       res.status(201).json({
         success: true,
