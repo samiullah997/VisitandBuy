7eb5a11be8a3c367df129cd2e00806e2c8cb627b
Samiullah <samiullahk997@gmail.com>
2024-01-22 23:31:02 +0500

    Update Hero.jsx file

--- frontend/src/components/Route/Hero/Hero.jsx
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -1,37 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "../../../styles/styles";
+import visitandbuy_banner from "../../../Assests/visitandbuy_banner.jpg";
+import visitandbuy_mobile_banner from "../../../Assests/visitandbuy_mobile_banner.jpg";
 
 const Hero = () => {
   return (
     <div
-      className={`relative min-h-[70vh] 800px:min-h-[80vh] w-full bg-no-repeat ${styles.noramlFlex}`}
-      style={{
-        backgroundImage:
-          "url(https://themes.rslahmed.dev/rafcart/assets/images/banner-2.jpg)",
-      }}
+      className={`relative min-h-[40vh] 800px:min-h-[80vh] w-full bg-no-repeat ${styles.noramlFlex}`}
     >
-      <div className={`${styles.section} w-[90%] 800px:w-[60%]`}>
-        <h1
-          className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#3d3a3a] font-[600] capitalize`}
-        >
-          Your WishList
-          <br /> Our Command
-        </h1>
-        <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#000000ba]">
-          Indulge in a personalized shopping experience like never before with
-          'Your Wishlist, Our Command.'
-          <br />
-          Simply curate your wishlist, and watch as we turn your dreams into
-          reality.<br /> At our e-commerce haven, your desires take center stage.
-        </p>
-        <Link to="/products" className="inline-block">
-          <div className={`${styles.button} mt-5`}>
-            <span className="text-[#fff] font-[Poppins] text-[18px]">
-              Shop Now
-            </span>
-          </div>
-        </Link>
+      {/* Mobile Version: Show on screens smaller than 800px */}
+      <div className={`md:hidden ${styles.section} w-full`}>
+        {/* Replace the mobile banner image here */}
+        <img
+          src={visitandbuy_mobile_banner}
+          alt="Mobile Banner"
+          className="w-full h-full object-cover"
+        />
+      </div>
+
+      {/* Desktop Version: Show on screens 800px and larger */}
+      <div className={`hidden md:block ${styles.section} w-full hidden`}>
+        {/* Replace the desktop banner content here */}
+        {/* <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div> */}
+        <img
+          src={visitandbuy_banner}
+          alt="Desktop Banner"
+          className="w-full h-full object-cover"
+        />
       </div>
     </div>
   );
